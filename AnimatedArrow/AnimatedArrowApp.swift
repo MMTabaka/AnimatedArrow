9308a0c01215d10fb06d995913eb99b24c0376aa
Milosz Tabaka <miloszmtabaka@gmail.com>
2022-10-31 22:14:45 +0000

    Initial Commit

---
+++ AnimatedArrow/AnimatedArrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedArrowApp.swift
+//  AnimatedArrow
+//
+//  Created by Miłosz Tabaka on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedArrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
